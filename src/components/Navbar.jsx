7652ceaab5cfd3d07c967a7de256c4ea875be728
LesRhabilleurs <joker2822.br@gmail.com>
2026-03-08 21:39:42 +0100

    Add Navbar component with navigation links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,14 @@
+import { Link } from "react-router-dom"
 
+export default function Navbar() {
+  return (
+    <nav>
+      <h2>Magic Flowers 🌿</h2>
+
+      <div>
+        <Link to="/">Accueil</Link>
+        <Link to="/shop">Boutique</Link>
+      </div>
+    </nav>
+  )
+}
