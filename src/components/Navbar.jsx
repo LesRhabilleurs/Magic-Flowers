14046c670aacdbacd0ec452a59fb27b86cbc0bc8
LesRhabilleurs <joker2822.br@gmail.com>
2026-03-08 22:20:17 +0100

    Update Navbar component to include Cart and logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,20 @@
 import { Link } from "react-router-dom"
+import Cart from "./Cart"       // le composant panier
+import logo from "../assets/logo.png"  // chemin vers ton logo
 
-export default function Navbar() {
+export default function Navbar({ cart, setCart }) {
   return (
-    <nav>
-      <h2>Magic Flowers 🌿</h2>
+    <nav className="navbar">
+      <Link to="/" className="logo-link">
+        <img src={logo} alt="Magic Flowers" className="logo" />
+      </Link>
 
-      <div>
+      <div className="nav-links">
         <Link to="/">Accueil</Link>
         <Link to="/shop">Boutique</Link>
       </div>
+
+      <Cart cart={cart} setCart={setCart} />
     </nav>
   )
 }
