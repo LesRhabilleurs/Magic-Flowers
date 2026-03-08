72c572cc7d901d060de4822efc0ca9db78a4f433
LesRhabilleurs <joker2822.br@gmail.com>
2026-03-08 21:48:57 +0100

    Add product data for Fleur CBD items

--- src/data/products.js
+++ src/data/products.js
@@ -1,1 +1,25 @@
+const products = [
+  {
+    id: 1,
+    name: "Fleur CBD Premium",
+    price: 12.99,
+    description: "Fleur CBD suisse de qualité supérieure",
+    image: "/images/fleur1.jpg"
+  },
+  {
+    id: 2,
+    name: "Fleur CBD Relax",
+    price: 9.99,
+    description: "Pour se détendre après une longue journée",
+    image: "/images/fleur2.jpg"
+  },
+  {
+    id: 3,
+    name: "Fleur CBD Énergie",
+    price: 11.49,
+    description: "Pour un boost naturel et léger",
+    image: "/images/fleur3.jpg"
+  }
+]
 
+export default products
