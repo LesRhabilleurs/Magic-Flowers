30c8ec014d5408c5d366607a2c3b4b596a0af720
LesRhabilleurs <joker2822.br@gmail.com>
2026-03-08 21:40:18 +0100

    Add Navbar component to App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Shop from "./pages/Shop"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <BrowserRouter>
 
-      <Routes>
+      <Navbar />
 
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
-
       </Routes>
 
     </BrowserRouter>
