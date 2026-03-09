54f1cfb3c439c1f66698998fedea8318119ecff4
LesRhabilleurs <joker2822.br@gmail.com>
2026-03-09 23:11:04 +0100

    Fix image paths in products data
    Updated image paths for product entries.

--- src/data/products.js
+++ src/data/products.js
@@ -4,21 +4,21 @@ const products = [
     name: "Fleur CBD High Society (10G)",
     price: 69.90,
     description: "Fleurs de CBD suisse de qualité supérieure au gout Bonbon",
-    image: "/images/fleur1.jpeg"
+    image: "/fleur1.jpeg"
   },
   {
     id: 2,
-    name: "Fleur CBD High Society (épices)",
-    price: 19.90,
-    description: "Fleurs de CBD suisse de qualité supérieure",
-    image: "/images/fleur2.jpg"
+    name: "Fleur CBD High Society (10G)",
+    price: 69.90,
+    description: "Fleurs de CBD suisse de qualité supérieure au gout épicé",
+    image: "/fleur2.jpg"
   },
   {
     id: 3,
     name: "Fleur CBD High Society (citriques)",
     price: 19.90,
     description: "Fleurs de CBD suisse de qualité supérieure",
-    image: "/images/fleur3.jpg"
+    image: "/fleur3.jpg"
   }
 ]
 
