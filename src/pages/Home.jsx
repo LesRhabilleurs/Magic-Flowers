125303c01a8be54c7ec172f671a04e8496ffb353
LesRhabilleurs <joker2822.br@gmail.com>
2026-03-09 22:35:40 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Magic Flowers 🌿</h1>
-      <p>Bienvenue sur notre boutique CBD</p>
+      <h1>Magic Flowers </h1> <p>Bienvenue sur notre boutique CBD</p>
     </div>
   )
 }
