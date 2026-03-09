1272d2d1ce99477557e78097e70d3a2c3c3c3bf9
LesRhabilleurs <joker2822.br@gmail.com>
2026-03-09 23:37:57 +0100

    Fix product prices in products.js
    Removed invalid characters from product prices.

--- src/data/products.js
+++ src/data/products.js
@@ -2,21 +2,21 @@ const products = [
   {
     id: 1,
     name: "Fleur CBD High Society (10G)",
-    price: 69.90.-,
+    price: 69.90,
     description: "Fleurs de CBD suisse de qualité supérieure au gout Bonbon",
     image: "/fleur1.jpeg"
   },
   {
     id: 2,
     name: "Fleur CBD High Society (10G)",
-    price: 69.90.-,
+    price: 69.90,
     description: "Fleurs de CBD suisse de qualité supérieure au gout épicé",
     image: "/fleur2.jpeg"
   },
   {
     id: 3,
     name: "Fleur CBD High Society (citriques)",
-    price: 19.90.-,
+    price: 19.90,
     description: "Fleurs de CBD suisse de qualité supérieure",
     image: "/fleur3.jpg"
   }
