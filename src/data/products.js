fd819789b5e6cc1ad7889cf39ead3dc772036bc7
LesRhabilleurs <joker2822.br@gmail.com>
2026-03-10 22:26:27 +0100

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -1,21 +1,21 @@
 const products = [
   {
     id: 1,
-    name: "Fleur CBD High Society (10G)",
+    name: "Fleur CBD High Society (10g)",
     price: 69.90,
     description: "Fleurs de CBD suisse de qualité supérieure au gout Bonbon",
     image: "/fleur1.jpeg"
   },
   {
     id: 2,
-    name: "Fleur CBD High Society (10G)",
+    name: "Fleur CBD High Society (10g)",
     price: 69.90,
     description: "Fleurs de CBD suisse de qualité supérieure au gout épicé",
     image: "/fleur2.jpeg"
   },
   {
     id: 3,
-    name: "Fleur CBD High Society (citriques)",
+    name: "Fleur CBD High Society (3.5g)",
     price: 19.90,
     description: "Fleurs de CBD suisse de qualité supérieure au gout citronné",
     image: "/fleur3.jpeg"
