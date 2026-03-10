263de09b4b2876bcbd4e0f7486e638b36a893ff7
LesRhabilleurs <joker2822.br@gmail.com>
2026-03-11 00:10:33 +0100

    Update Shop.jsx

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,25 +1,49 @@
 import React from "react";
 import products from "../data/products";
-import "./Shop.css"; // CSS spécifique à la page
+import "./Shop.css";
 
-export default function Shop() {
+export default function Shop({ cart, setCart }) {
+
+  const addToCart = (product) => {
+    const existingProduct = cart.find((item) => item.id === product.id);
+
+    if (existingProduct) {
+      setCart(
+        cart.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      );
+    } else {
+      setCart([...cart, { ...product, quantity: 1 }]);
+    }
+  };
+
   return (
     <section className="shop">
-      {/* Titre centré */}
+
+      {/* Titre */}
       <h1 className="shop-title">Nos Produits</h1>
 
-      {/* Grille des produits */}
+      {/* Grille produits */}
       <div className="grid">
         {products.map((product) => (
           <div key={product.id} className="product">
+
             <img src={product.image} alt={product.name} />
             <h3>{product.name}</h3>
             <p>{product.description}</p>
             <p>CHF {product.price.toFixed(2)}</p>
-            <button>Ajouter au panier</button>
+
+            <button onClick={() => addToCart(product)}>
+              Ajouter au panier
+            </button>
+
           </div>
         ))}
       </div>
+
     </section>
   );
 }
