42569fc52040185e35ac1df2be43b2fbd801a4b0
LesRhabilleurs <joker2822.br@gmail.com>
2026-03-10 09:21:01 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 import Home from "./pages/Home"
 import Shop from "./pages/Shop"
 import Navbar from "./components/Navbar"
+
 import "./App.css"
 
 function App() {
@@ -10,13 +12,18 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Navbar cart={cart} setCart={setCart} />
+      <Navbar cart={cart} />
+
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop cart={cart} setCart={setCart} />} />
+
+        <Route
+          path="/shop"
+          element={<Shop cart={cart} setCart={setCart} />}
+        />
       </Routes>
     </BrowserRouter>
   )
 }
 
-export default App
+export default App App
