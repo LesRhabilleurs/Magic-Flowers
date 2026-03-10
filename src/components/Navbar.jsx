e65583c6a0c1665277d079e1aa18bbbc5a9b364d
LesRhabilleurs <joker2822.br@gmail.com>
2026-03-10 09:49:46 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,33 @@
-import { Link } from "react-router-dom"
-import Cart from "./Cart"       // le composant panier
-import logo from "../assets/logo.png"  // chemin vers ton logo
+import { Link } from "react-router-dom";
+import Cart from "./Cart";
+import logo from "../assets/logo.png";
 
 export default function Navbar({ cart, setCart }) {
   return (
     <nav className="navbar">
+      
+      {/* Logo */}
       <Link to="/" className="logo-link">
         <img src={logo} alt="Magic Flowers" className="logo" />
       </Link>
 
+      {/* Liens de navigation */}
       <div className="nav-links">
         <Link to="/">Accueil</Link>
         <Link to="/shop">Boutique</Link>
-        <Link to="/">Accesoires</Link>
-        <Link to="/">FAQ</Link>
+        <Link to="/accessoires">Accessoires</Link>
+        <Link to="/faq">FAQ</Link>
       </div>
 
+      {/* Panier */}
       <Cart cart={cart} setCart={setCart} />
+
+      {/* Burger menu pour mobile */}
+      <div className="burger-menu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </nav>
-  )
+  );
 }
