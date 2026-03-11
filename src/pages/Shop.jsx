21771eb3bfca94c58c13facf2759de046e1e5582
LesRhabilleurs <joker2822.br@gmail.com>
2026-03-11 12:55:04 +0100

    Update Shop.jsx

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,32 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import products from "../data/products";
 import "./Shop.css";
+import { motion } from "framer-motion";
 
 export default function Shop({ cart, setCart }) {
 
-  const addToCart = (product) => {
-    const existingProduct = cart.find((item) => item.id === product.id);
+  const [flyingProduct, setFlyingProduct] = useState(null);
 
-    if (existingProduct) {
-      setCart(
-        cart.map((item) =>
-          item.id === product.id
-            ? { ...item, quantity: item.quantity + 1 }
-            : item
-        )
-      );
-    } else {
-      setCart([...cart, { ...product, quantity: 1 }]);
-    }
+  const addToCart = (product) => {
+
+    setFlyingProduct(product);
+
+    setTimeout(() => {
+      const existingProduct = cart.find((item) => item.id === product.id);
+
+      if (existingProduct) {
+        setCart(
+          cart.map((item) =>
+            item.id === product.id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          )
+        );
+      } else {
+        setCart([...cart, { ...product, quantity: 1 }]);
+      }
+
+      setFlyingProduct(null);
+
+    }, 600);
   };
 
   return (
     <section className="shop">
 
-      {/* Titre */}
       <h1 className="shop-title">Nos Produits</h1>
 
-      {/* Grille produits */}
       <div className="grid">
         {products.map((product) => (
           <div key={product.id} className="product">
@@ -43,6 +52,23 @@ export default function Shop({ cart, setCart }) {
           </div>
         ))}
       </div>
+
+      {flyingProduct && (
+        <motion.img
+          src={flyingProduct.image}
+          initial={{ x: 0, y: 0, scale: 1, opacity: 1 }}
+          animate={{ x: 500, y: -300, scale: 0.2, opacity: 0 }}
+          transition={{ duration: 0.6 }}
+          style={{
+            position: "fixed",
+            bottom: "200px",
+            left: "40%",
+            width: "100px",
+            pointerEvents: "none",
+            zIndex: 1000
+          }}
+        />
+      )}
 
     </section>
   );
