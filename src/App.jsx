e83d9e126b171155c1511f7c33e36401f1edfaf5
LesRhabilleurs <joker2822.br@gmail.com>
2026-03-11 15:31:21 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import Home from "./pages/Home"
 import Shop from "./pages/Shop"
-import FAQ from "./pages/FAQ";
+import Faq from "./pages/FAQ";
 import Navbar from "./components/Navbar"
 
 import "./App.css"
