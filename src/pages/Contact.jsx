bb78b9435c72f9f07b3ac1574c81a295ebbe66a4
LesRhabilleurs <joker2822.br@gmail.com>
2026-03-12 10:09:18 +0100

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -66,7 +66,7 @@ export default function Contact() {
 
     <div className="contact-info">
       <h3>Email</h3>
-      <p>contact@tonsitecbd.com</p>
+      <p>magic-flowers@info.ch.com</p>
 
       <h3>Support</h3>
       <p>Lundi - Vendredi : 9h - 18h</p>
